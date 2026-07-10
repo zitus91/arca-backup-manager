371eb40d15da8e400d7fdd3e5d481e41ce3e51a2
zitus91 <francesco.zito@me.com>
2026-07-10 08:29:50 +0200

    align

--- resources/js/echo.js
+++ resources/js/echo.js
@@ -1,8 +1,3 @@
-import axios from 'axios';
-window.axios = axios;
-
-window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-
 import Echo from 'laravel-echo';
 
 import Pusher from 'pusher-js';
@@ -17,11 +12,3 @@ window.Echo = new Echo({
     forceTLS: (import.meta.env.VITE_REVERB_SCHEME ?? 'https') === 'https',
     enabledTransports: ['ws', 'wss'],
 });
-
-/**
- * Echo exposes an expressive API for subscribing to channels and listening
- * for events that are broadcast by Laravel. Echo and event broadcasting
- * allow your team to quickly build robust real-time web applications.
- */
-
-import './echo';
